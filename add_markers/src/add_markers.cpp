bf8285c26ba325f847cad157478a8a639b0382ca
robond <felixisaacp@gmail.com>
2019-06-18 01:37:56 -0700

    Implement marker handling service

--- add_markers/src/add_markers.cpp
+++ add_markers/src/add_markers.cpp
@@ -1,85 +1,90 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
+#include <geometry_msgs/Pose.h>
+#include <std_srvs/Empty.h>
+#include "add_markers/MarkerData.h"
+
+class AddMarkersService
+{
+public:
+	AddMarkersService()
+	{
+		ROS_INFO_STREAM("Setup add_markers service class.");
+
+		m_pubMarker = m_hNode.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+
+		m_servAdd = m_hNode.advertiseService("/add_markers/add_marker", &AddMarkersService::addMarkerHandler, this);
+		m_servHide = m_hNode.advertiseService("/add_markers/hide_marker", &AddMarkersService::hideMarkerHandler, this);
+
+		// setup basic marker details
+		// only need to change the pose
+		m_marker.header.frame_id = "map";
+
+		m_marker.ns = "goal";
+		m_marker.id = 0;
+
+    	m_marker.type = visualization_msgs::Marker::SPHERE;
+
+		m_marker.pose.orientation.x = 0.0;
+		m_marker.pose.orientation.y = 0.0;
+		m_marker.pose.orientation.z = 0.0;
+		m_marker.pose.orientation.w = 1.0;
+
+		m_marker.scale.x = 0.5;
+		m_marker.scale.y = 0.5;
+		m_marker.scale.z = 0.5;
+
+		m_marker.color.r = 0.988f;
+		m_marker.color.g = 0.552f;
+		m_marker.color.b = 0.113f;
+		m_marker.color.a = 1.0;
+
+		m_marker.lifetime = ros::Duration();
+
+		ROS_INFO("Ready to set marker!");
+	}
+
+private:
+	bool addMarkerHandler(add_markers::MarkerData::Request& req, add_markers::MarkerData::Response& res)
+	{
+		m_marker.action = visualization_msgs::Marker::ADD;
+
+		m_marker.pose.position = req.position;
+
+		m_pubMarker.publish(m_marker);
+
+		res.msg_feedback = "Marker set - x: " + std::to_string(req.position.x) + " y: " + std::to_string(req.position.x) + " z: " + std::to_string(req.position.z) + ".";
+
+		ROS_INFO_STREAM(res.msg_feedback);
+
+		return true;
+	}
+
+	bool hideMarkerHandler(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res)
+	{
+		m_marker.action = visualization_msgs::Marker::DELETE;
+
+		m_pubMarker.publish(m_marker);
+
+		ROS_INFO_STREAM("Marker removed.");
+
+		return true;
+	}
+
+	ros::NodeHandle		m_hNode;
+	ros::Publisher		m_pubMarker;
+	ros::ServiceServer	m_servAdd;
+	ros::ServiceServer	m_servHide;
+	visualization_msgs::Marker m_marker;
+};
 
 int main( int argc, char** argv )
 {
-  ros::init(argc, argv, "add_markers");
-  ros::NodeHandle n;
-  ros::Rate r(1);
-  ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+	ros::init(argc, argv, "add_markers");
 
-  // Set our initial shape type to be a cube
-  uint32_t shape = visualization_msgs::Marker::CUBE;
+	AddMarkersService obj;
 
-  while (ros::ok())
-  {
-    visualization_msgs::Marker marker;
-    // Set the frame ID and timestamp.  See the TF tutorials for information on these.
-    marker.header.frame_id = "map";
-    marker.header.stamp = ros::Time::now();
+	ros::spin();
 
-    // Set the namespace and id for this marker.  This serves to create a unique ID
-    // Any marker sent with the same namespace and id will overwrite the old one
-    marker.ns = "basic_shapes";
-    marker.id = 0;
-
-    // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
-    marker.type = shape;
-
-    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-    marker.action = visualization_msgs::Marker::ADD;
-
-    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-    marker.pose.position.x = 0;
-    marker.pose.position.y = 0;
-    marker.pose.position.z = 0;
-    marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = 1.0;
-
-    // Set the scale of the marker -- 1x1x1 here means 1m on a side
-    marker.scale.x = 1.0;
-    marker.scale.y = 1.0;
-    marker.scale.z = 1.0;
-
-    // Set the color -- be sure to set alpha to something non-zero!
-    marker.color.r = 0.0f;
-    marker.color.g = 1.0f;
-    marker.color.b = 0.0f;
-    marker.color.a = 1.0;
-
-    marker.lifetime = ros::Duration();
-
-    // Publish the marker
-    while (marker_pub.getNumSubscribers() < 1)
-    {
-      if (!ros::ok())
-      {
-        return 0;
-      }
-      ROS_WARN_ONCE("Please create a subscriber to the marker");
-      sleep(1);
-    }
-    marker_pub.publish(marker);
-
-    // Cycle between different shapes
-    switch (shape)
-    {
-    case visualization_msgs::Marker::CUBE:
-      shape = visualization_msgs::Marker::SPHERE;
-      break;
-    case visualization_msgs::Marker::SPHERE:
-      shape = visualization_msgs::Marker::ARROW;
-      break;
-    case visualization_msgs::Marker::ARROW:
-      shape = visualization_msgs::Marker::CYLINDER;
-      break;
-    case visualization_msgs::Marker::CYLINDER:
-      shape = visualization_msgs::Marker::CUBE;
-      break;
-    }
-
-    r.sleep();
-  }
+	return 0;
 }
