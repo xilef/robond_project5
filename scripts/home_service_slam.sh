6ae6bc19dd414b9c8af1192ae52651845d7ef214
robond <felixisaacp@gmail.com>
2019-07-11 08:13:38 -0700

    Fixed the incorrect link point for the kinect camera. Implement the home_service robot using rtabmap for the localization.

--- scripts/home_service_slam.sh
+++ scripts/home_service_slam.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xterm  -e  " source ~/workspace/catkin_ws/devel/setup.bash; roslaunch my_robot world.launch" &
 sleep 10
-xterm  -e  " source ~/workspace/catkin_ws/devel/setup.bash; roslaunch my_robot amcl.launch" &
+xterm  -e  " source ~/workspace/catkin_ws/devel/setup.bash; roslaunch my_robot localization.launch" &
 sleep 15
 xterm  -e  " source ~/workspace/catkin_ws/devel/setup.bash; rosrun add_markers add_markers" &
 sleep 5
